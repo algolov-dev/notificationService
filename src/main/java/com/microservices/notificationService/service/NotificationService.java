ada5fc4e9152794a4e82923cc391da6e55575718
Sasha <55playn77@gmail.com>
2024-12-03 10:09:20 +0300

    Initial commit

---
+++ src/main/java/com/microservices/notificationService/service/NotificationService.java
@@ -0,0 +1,45 @@
+package com.microservices.notificationService.service;
+
+import com.microservices.orderService.event.OrderPlacedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class NotificationService {
+    private final JavaMailSender javaMailSender;
+
+    @KafkaListener(topics = "order-placed")
+    public void listen(OrderPlacedEvent orderPlacedEvent) {
+        log.info("Got Message from order-placed topic {}", orderPlacedEvent);
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setFrom("springshop@email.com");
+            messageHelper.setTo(orderPlacedEvent.getEmail());
+            messageHelper.setSubject("Your order from Spring Shop");
+            messageHelper.setText(String.format("""
+                    Hi
+                    
+                    Your order with order number %s is now placed successfuly.
+                    
+                    Best Regards
+                    Spring Shop
+                    """,
+                    orderPlacedEvent.getOrderNumber()));
+        };
+        try {
+            javaMailSender.send(messagePreparator);
+            log.info("Order notification email send!");
+        } catch( MailException e) {
+            log.error("Exception occurred with sending mail", e);
+            throw new RuntimeException(e);
+        }
+    }
+}
